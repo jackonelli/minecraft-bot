7f3fe344506c57e36c5012b68f0a16bcbf32decd
georg-bn <georgbokman@outlook.com>
2020-08-21 15:03:41 +0200

    added a main method...

--- parse_missions.py
+++ parse_missions.py
@@ -6,15 +6,20 @@ import glob
 # below should be your path to the 11.2 LTH world
 PATH_TO_LTH_WORLD = os.environ['MINERL_PATH_TO_LTH_WORLD']
 
-if not os.path.isdir("my_mission_xmls"):
-    os.mkdir("my_mission_xmls")
+def parse_all():
+    if not os.path.isdir("my_mission_xmls"):
+        os.mkdir("my_mission_xmls")
 
-for xml_path in glob.glob("mission_xmls/*.xml"):
-    xml_file = open(xml_path, "r")
-    xml = xml_file.read()
-    xml_file.close()
+    for xml_path in glob.glob("mission_xmls/*.xml"):
+        xml_file = open(xml_path, "r")
+        xml = xml_file.read()
+        xml_file.close()
 
-    xml = xml.replace("$(MINERL_PATH_TO_LTH_WORLD)", PATH_TO_LTH_WORLD)
-    write_file = open("my_" + xml_path, "w+")
-    write_file.write(xml)
-    write_file.close()
+        xml = xml.replace("$(MINERL_PATH_TO_LTH_WORLD)", PATH_TO_LTH_WORLD)
+        write_file = open("my_" + xml_path, "w+")
+        write_file.write(xml)
+        write_file.close()
+
+
+if __name__ == "__main__":
+    parse_all()
